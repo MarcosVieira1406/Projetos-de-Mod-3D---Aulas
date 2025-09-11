3a0578910ec521cdbd4f54123066299c2f585715
Marcos Eduardo Surita Vieira <mesuritavieira@gmail.com>
2025-09-11 20:41:46 -0300

    Mudancas no codigo

--- 20250904/P1-Linha-Triangulo/main.cpp
+++ 20250904/P1-Linha-Triangulo/main.cpp
@@ -1,16 +1,61 @@
 //Autor: Marcos Eduardo - Projeto: Linha e triangulo 04/09/2025
 
-
 #include <GL/glut.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
 
-static void display(void)
+float r,g,b,x,y;
+bool check = true;
+
+GLfloat escala = 1;
+
+void mouse(int button, int state, int mousex, int mousey)
+{
+    if(button==GLUT_LEFT_BUTTON)
+
+    {
+        check=true;
+        x = mousex;
+        y = 480-mousey;
+        r = (rand()%10)/10.0;
+        g = (rand()%10)/10.0;
+        b = (rand()%10)/10.0;
+    }
+    else if(button==GLUT_RIGHT_BUTTON && state==GLUT_DOWN)
+    {
+        glClearColor(1, 1, 1, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        check = false;
+    }
+    glutPostRedisplay();
+}
+
+static void desenha(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(r,g,b);
+    glPointSize(50);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    //define a area de enquadramento do sistema
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+    if(check)
+    {
+        glBegin(GL_POINTS);
+            glVertex2i(x,y);
+        glEnd();
+    }
+    glFlush();
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //define a area de enquadramento do sistema/cena
     gluOrtho2D(-3, 3, -3, 3);
+
+    glScalef(escala, escala, 0);
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glLineWidth(10);
@@ -30,15 +75,29 @@ static void display(void)
         glFlush();
 }
 
+void listeningKey (unsigned char tecla, GLint x, GLint y){
+    switch(tecla){
+        case '+': escala++;
+            break;
+        case '-': escala--;
+            break;
+    }
+    desenha();
+}
+
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     glutInit(&argc, argv);
     glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
+    glutInitWindowPosition(300,100);
+    glutCreateWindow("Ola GLUT");
+    glutKeyboardFunc(listeningKey);
+    glutDisplayFunc(desenha);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutMouseFunc(mouse);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutCreateWindow("GLUT Shapes");
-    glClearColor(0, 1, 0, 0);
-    glutDisplayFunc(display);
+    glClearColor(0, 0, 1, 0);
     glutMainLoop();
     return EXIT_SUCCESS;
 }
